b3a7587fb41b489ee4d6af84852333f035d32d3a
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-11-25 22:12:27 +0400

    Update build.gradle.kts

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -23,12 +23,14 @@ repositories {
 	mavenCentral()
 }
 
+val springCloudVersion: String by extra("2023.0.3")
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
-
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
@@ -37,6 +39,12 @@ dependencies {
 	annotationProcessor ("org.mapstruct:mapstruct-processor:1.5.3.Final")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+	}
 }
 
 tasks.withType<Test> {
