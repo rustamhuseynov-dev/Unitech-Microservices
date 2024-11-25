7892c01b0cc6f87e792b4a964e7f8497654c12b1
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-11-25 16:59:21 +0400

    Create MsAuth1Application.java

---
+++ msauth1/src/main/java/az/edu/turing/msauth1/MsAuth1Application.java
@@ -0,0 +1,13 @@
+package az.edu.turing.msauth1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuth1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAuth1Application.class, args);
+    }
+
+}
