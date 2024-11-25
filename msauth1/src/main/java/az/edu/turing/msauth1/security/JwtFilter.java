f370c314c917d685d3def26dc28f298ebcbc0089
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-11-25 16:59:30 +0400

    Create JwtFilter.java

---
+++ msauth1/src/main/java/az/edu/turing/msauth1/security/JwtFilter.java
@@ -0,0 +1,40 @@
+package az.edu.turing.msauth1.security;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            try {
+                if (jwtUtil.validateToken(token)) {
+
+                }
+            } catch (ExpiredJwtException e) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("JWT Token Expired");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
