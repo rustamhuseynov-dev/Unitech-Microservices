29739461e18000b17c9931e029f325bc03abccf6
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-05 00:53:58 +0400

    Update UserController.java

--- ms-auth/src/main/java/com/rustam/unitech/controller/UserController.java
+++ ms-auth/src/main/java/com/rustam/unitech/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.rustam.unitech.controller;
 
 import com.rustam.unitech.dto.request.UserRequest;
+import com.rustam.unitech.dto.request.UserUpdateRequest;
 import com.rustam.unitech.dto.response.UserResponse;
 import com.rustam.unitech.service.UserService;
 import lombok.AccessLevel;
@@ -8,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/user")
@@ -25,4 +23,10 @@ public class UserController {
     public ResponseEntity<UserResponse> save(@RequestBody UserRequest userRequest){
         return new ResponseEntity<>(userService.save(userRequest), HttpStatus.CREATED);
     }
+
+    @PutMapping(path = "/update")
+    public ResponseEntity<UserResponse> update(@RequestBody UserUpdateRequest userUpdateRequest){
+        return new ResponseEntity<>(userService.update(userUpdateRequest),HttpStatus.OK);
+    }
+
 }
