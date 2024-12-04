4814fd85eadcbda7b710de1ae3e2569ac46a8418
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-04 21:38:59 +0400

    trash

--- msauth1/src/main/java/az/edu/turing/msauth1/model/enums/UserRole.java
+++
@@ -1,6 +0,0 @@
-package az.edu.turing.msauth1.model.enums;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
--- user/src/main/java/org/example/msuser/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.msuser.Repository;
-
-import org.example.msuser.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
