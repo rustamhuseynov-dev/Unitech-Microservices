1d3964e53b71f0a57df0b9ed79895f9f1b1cdc8f
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-04 21:38:49 +0400

    Create MsNotificationServiceApplicationTests.java

---
+++ ms-notification-service/src/test/java/com/rustam/ms_notification_service/MsNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rustam.ms_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
