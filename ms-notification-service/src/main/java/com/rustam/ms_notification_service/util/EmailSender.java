09a7ed9d9636357a4a813647513a810552a4dfc7
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-05 00:54:27 +0400

    Create EmailSender.java

---
+++ ms-notification-service/src/main/java/com/rustam/ms_notification_service/util/EmailSender.java
@@ -0,0 +1,21 @@
+package com.rustam.ms_notification_service.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailSender {
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
