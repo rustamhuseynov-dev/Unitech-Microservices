24d5ce1239d0f2163486be66962a30e4f7cf0bc9
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-05 00:54:18 +0400

    Update SendMailDto.java

--- ms-notification-service/src/main/java/com/rustam/ms_notification_service/dto/SendMailDto.java
+++ ms-notification-service/src/main/java/com/rustam/ms_notification_service/dto/SendMailDto.java
@@ -1,4 +1,17 @@
 package com.rustam.ms_notification_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SendMailDto {
+
+    private String username;
+
+    private String password;
 }
