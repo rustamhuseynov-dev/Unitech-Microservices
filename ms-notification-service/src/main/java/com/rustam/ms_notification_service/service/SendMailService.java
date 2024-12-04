f09e19b27140e0e56a2196e9eefc268c23e222ff
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-04 22:15:41 +0400

    Create SendMailService.java

---
+++ ms-notification-service/src/main/java/com/rustam/ms_notification_service/service/SendMailService.java
@@ -0,0 +1,11 @@
+package com.rustam.ms_notification_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SendMailService {
+
+
+}
