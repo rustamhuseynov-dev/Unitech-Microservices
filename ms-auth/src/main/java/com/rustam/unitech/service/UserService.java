3f94424407de71dbe1b80977167a434e9f9fc9fa
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-05 17:37:09 +0400

    Update UserService.java

--- ms-auth/src/main/java/com/rustam/unitech/service/UserService.java
+++ ms-auth/src/main/java/com/rustam/unitech/service/UserService.java
@@ -3,6 +3,7 @@ package com.rustam.unitech.service;
 import com.rustam.unitech.dto.request.UserRequest;
 import com.rustam.unitech.dto.request.UserUpdateRequest;
 import com.rustam.unitech.dto.response.UserResponse;
+import com.rustam.unitech.exception.custom.ExistsException;
 import com.rustam.unitech.exception.custom.UserNotFoundException;
 import com.rustam.unitech.mapper.UserMapper;
 import com.rustam.unitech.model.User;
@@ -12,10 +13,13 @@ import com.rustam.unitech.service.kafka.KafkaProducerService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +28,7 @@ public class UserService {
 
     UserRepository userRepository;
     PasswordEncoder passwordEncoder;
+    ModelMapper modelMapper;
     UserMapper userMapper;
     KafkaProducerService kafkaProducerService;
 
@@ -44,9 +49,35 @@ public class UserService {
     public UserResponse update(UserUpdateRequest userUpdateRequest) {
         User user = userRepository.findById(userUpdateRequest.getId())
                 .orElseThrow(() -> new UserNotFoundException("No such user found."));
-        UserResponse response = userMapper.toDto(user);
+        boolean exists = findAll().stream()
+                .map(User::getUsername)
+                .anyMatch(existingUsername -> existingUsername.equals(userUpdateRequest.getUsername()));
+        if (exists){
+            throw new ExistsException("This username is already taken.");
+        }
+        modelMapper.map(userUpdateRequest,user);
+        user.setPassword(passwordEncoder.encode(userUpdateRequest.getPassword()));
         userRepository.save(user);
+        UserResponse response = UserResponse.builder()
+                .name(user.getName())
+                .username(user.getUsername())
+                .build();
         kafkaProducerService.sendMessage(response);
         return response;
+    }
+
+    public List<UserResponse> readAll() {
+        List<User> users = findAll();
+        return userMapper.toResponses(users);
+    }
+
+    private List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public UserResponse read(UUID id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("No such user found."));
+        return userMapper.toResponse(user);
     }
 }
