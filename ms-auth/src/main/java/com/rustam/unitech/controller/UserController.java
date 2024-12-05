7ec054710cd5aa06bcec9b9a7635f204afc38d9c
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-05 17:36:46 +0400

    Update UserController.java

--- ms-auth/src/main/java/com/rustam/unitech/controller/UserController.java
+++ ms-auth/src/main/java/com/rustam/unitech/controller/UserController.java
@@ -11,6 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping(path = "/api/v1/user")
 @RequiredArgsConstructor
@@ -22,6 +25,21 @@ public class UserController {
     @PostMapping(path = "/register")
     public ResponseEntity<UserResponse> save(@RequestBody UserRequest userRequest){
         return new ResponseEntity<>(userService.save(userRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/update")
+    public ResponseEntity<UserResponse> update(@RequestBody UserUpdateRequest userUpdateRequest){
+        return new ResponseEntity<>(userService.update(userUpdateRequest),HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/read-all")
+    public ResponseEntity<List<UserResponse>> readAll(){
+        return new ResponseEntity<>(userService.readAll(),HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/read/{id}")
+    public ResponseEntity<UserResponse> read(@PathVariable UUID id){
+        return new ResponseEntity<>(userService.read(id),HttpStatus.OK);
     }
 
 }
