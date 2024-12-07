70b6d9caaf70a9ef8013fad9d23302906194e160
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-07 19:05:05 +0400

    Create KafkaConsumerConfig.java

---
+++ ms-account/src/main/java/com/rustam/ms_account/config/KafkaConsumerConfig.java
@@ -0,0 +1,68 @@
+package com.rustam.ms_account.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@RequiredArgsConstructor
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String host;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Bean
+    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory(){
+        var factory = new ConcurrentKafkaListenerContainerFactory<String,Object>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<?> customKafkaListenerContainerFactory(){
+        var factory = new ConcurrentKafkaListenerContainerFactory<String,Object>();
+        factory.setConsumerFactory(customConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String,Object> consumerFactory(){
+        JsonDeserializer<Object> deserializer = new JsonDeserializer<>(Object.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+        Map<String,Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,host);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,deserializer);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,groupId);
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConsumerFactory<String,Object> customConsumerFactory(){
+        Map<String,Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,host);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,groupId);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+}
