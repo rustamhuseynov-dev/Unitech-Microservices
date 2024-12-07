96fc88bc970ec6bee2718c37be2616119f693075
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-07 23:44:40 +0400

    Update JwtUtil.java

--- ms-api-gateway/src/main/java/com/rustam/api_gateway/util/JwtUtil.java
+++ ms-api-gateway/src/main/java/com/rustam/api_gateway/util/JwtUtil.java
@@ -1,6 +1,9 @@
 package com.rustam.api_gateway.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,5 +25,26 @@ public class JwtUtil {
     private Key getSignKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isValidUserIdFromToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            // User ID və ya username-i əldə edin
+            String userId = claims.getSubject();
+
+            // Burada userId yoxlayın (null və ya boş deyil)
+            return userId != null && !userId.isEmpty();
+        } catch (ExpiredJwtException e) {
+            // Token vaxtı keçmişdir
+            return false; // Burada false qaytarılır
+        } catch (SignatureException | IllegalArgumentException e) {
+            // Token səhvdir
+            return false; // Burada false qaytarılır
+        }
     }
 }
