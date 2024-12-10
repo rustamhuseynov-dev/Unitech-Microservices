f4aac5f13a18bd6020897301aef499ca1ea9e1e3
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-10 12:05:30 +0400

    Update AccountRequest.java

--- ms-account/src/main/java/com/rustam/ms_account/dto/request/AccountRequest.java
+++ ms-account/src/main/java/com/rustam/ms_account/dto/request/AccountRequest.java
@@ -3,16 +3,16 @@ package com.rustam.ms_account.dto.request;
 import com.rustam.ms_account.dao.enums.AccountStatus;
 import com.rustam.ms_account.dao.enums.Currency;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class AccountRequest {
-
-    @NotBlank
-    private String username;
 
     private Currency currency;
 }
