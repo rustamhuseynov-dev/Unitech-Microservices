a6ec545969e13a662e1dd965493c1dc4a618aa34
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-11 00:14:56 +0400

    Update GlobalExceptionHandler.java

--- ms-account/src/main/java/com/rustam/ms_account/exception/GlobalExceptionHandler.java
+++ ms-account/src/main/java/com/rustam/ms_account/exception/GlobalExceptionHandler.java
@@ -4,17 +4,21 @@ import com.rustam.ms_account.dto.exception.ExceptionResponseMessages;
 import com.rustam.ms_account.exception.custom.ResponseNotFoundException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResponseNotFoundException.class)
