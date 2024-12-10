16c04c917883da881013931f5dd6db7b8b89d2fc
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-10 12:05:36 +0400

    Update AccountService.java

--- ms-account/src/main/java/com/rustam/ms_account/service/AccountService.java
+++ ms-account/src/main/java/com/rustam/ms_account/service/AccountService.java
@@ -5,18 +5,23 @@ import com.rustam.ms_account.dao.enums.AccountStatus;
 import com.rustam.ms_account.dao.repository.AccountRepository;
 import com.rustam.ms_account.dto.request.AccountIncreaseRequest;
 import com.rustam.ms_account.dto.request.AccountRequest;
+import com.rustam.ms_account.dto.request.UpdateAccountRequest;
 import com.rustam.ms_account.dto.response.AccountIncreaseResponse;
 import com.rustam.ms_account.dto.response.AccountResponse;
 import com.rustam.ms_account.mapper.AccountMapper;
 import com.rustam.ms_account.util.IbanService;
+import com.rustam.ms_account.util.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -30,14 +35,42 @@ public class AccountService {
 
     private final IbanService ibanService;
 
+    private final RestTemplate restTemplate;
+
+    private final JwtUtil jwtUtil;
+
+    private static final String AUTH_SERVICE_URL = "http://localhost:8085/api/v1/user/read";
+
     @Transactional
-    public AccountResponse createAccount(AccountRequest accountRequest) {
+    public AccountResponse createAccount(AccountRequest accountRequest,String token) {
+
+        token = token.replace("Bearer ", "");
+
+        UUID userId = UUID.fromString(jwtUtil.extractUsername(token));
+
+        log.info("token {}",userId);
+
+        String url = AUTH_SERVICE_URL + "/" + userId;
+
+        HttpHeaders headers = new HttpHeaders();
+        String accessToken = "Bearer " + token;
+        headers.set("Authorization", accessToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<AccountResponse> response = restTemplate.exchange(url, HttpMethod.GET, entity, AccountResponse.class);
+
+        if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
+            throw new RuntimeException("Failed to fetch user data from MS-Auth");
+        }
+        AccountResponse accountResponse = response.getBody();
 
         Account account = Account.builder()
-                .username(accountRequest.getUsername())
                 .currency(accountRequest.getCurrency())
                 .iban(ibanService.generateIbanForUser())
+                .username(accountResponse.getUsername())
                 .status(AccountStatus.ACTIVATED)
+                .customerId(userId)
                 .build();
         accountRepository.save(account);
         return accountMapper.toDto(account);
@@ -69,9 +102,9 @@ public class AccountService {
     }
 
     @Transactional
-    public AccountResponse updateAccount(AccountRequest accountRequest) {
-        Account account = accountRepository.findByUsername(accountRequest.getUsername());
-        account.setCurrency(accountRequest.getCurrency());
+    public AccountResponse updateAccount(UpdateAccountRequest updateAccountRequest) {
+        Account account = accountRepository.findByUsername(updateAccountRequest.getUsername());
+        account.setCurrency(updateAccountRequest.getCurrency());
         accountRepository.save(account);
         return accountMapper.toDto(account);
     }
